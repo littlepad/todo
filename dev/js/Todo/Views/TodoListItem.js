0f53cd50214448032f3cbcbfd56d0542e3d6006e
littlepad <littlepad@gmail.com>
2014-06-10 01:45:45 +0900

    add delete button

--- dev/js/Todo/Views/TodoListItem.js
+++ dev/js/Todo/Views/TodoListItem.js
@@ -1,16 +1,21 @@
 Todo.Views.TodoListItem = Todo.Views.ViewBase.extend({
 	tagName: 'li',
 	
-	tmpl: '<input type="checkbox"><span class="text"><%= text %></span>',
+	tmpl: '<p class="listItem"><input type="checkbox"><span class="text"><%= text %></span><a href="#" class="deleteButton">delete</a></p>',
 	
 	events: {
-		'change input[type="checkbox"]': 'onChangeCheckbox'
+		'change input[type="checkbox"]': 'onChangeCheckbox',
+		'mouseover p': 'onMouseover',
+		'mouseout p': 'onMouseout',
+		'click .deleteButton': 'onClick'
 	},
 	
 	initialize: function(options){
 		'use strict';
 		this.model = options.model;
 		this.render();
+
+		this.listenTo(this.model, 'destroy', this.remove);
 	},
 	
 	render: function() {
@@ -25,6 +30,21 @@ Todo.Views.TodoListItem = Todo.Views.ViewBase.extend({
 	onChangeCheckbox: function(e){
 		'use strict';
 		this.setCompleted(e.target);
+	},
+
+	onMouseover: function(e){
+		'use strict';
+		$(e.target).children('.deleteButton').addClass('showDeletebutton');
+	},
+
+	onMouseout: function(e) {
+		'use strict';
+		$(e.target).children('.deleteButton').removeClass('showDeletebutton');
+	},
+
+	onClick: function() {
+		'use strict';
+		this.model.destroy();
 	},
 	
 	setCompleted: function(target) {
--- dev/js/todo.js
+++ dev/js/todo.js
@@ -117,16 +117,21 @@ Todo.Views.TodoList = Todo.Views.ViewBase.extend({
 Todo.Views.TodoListItem = Todo.Views.ViewBase.extend({
 	tagName: 'li',
 	
-	tmpl: '<input type="checkbox"><span class="text"><%= text %></span>',
+	tmpl: '<p class="listItem"><input type="checkbox"><span class="text"><%= text %></span><a href="#" class="deleteButton">delete</a></p>',
 	
 	events: {
-		'change input[type="checkbox"]': 'onChangeCheckbox'
+		'change input[type="checkbox"]': 'onChangeCheckbox',
+		'mouseover p': 'onMouseover',
+		'mouseout p': 'onMouseout',
+		'click .deleteButton': 'onClick'
 	},
 	
 	initialize: function(options){
 		'use strict';
 		this.model = options.model;
 		this.render();
+
+		this.listenTo(this.model, 'destroy', this.remove);
 	},
 	
 	render: function() {
@@ -141,6 +146,21 @@ Todo.Views.TodoListItem = Todo.Views.ViewBase.extend({
 	onChangeCheckbox: function(e){
 		'use strict';
 		this.setCompleted(e.target);
+	},
+
+	onMouseover: function(e){
+		'use strict';
+		$(e.target).children('.deleteButton').addClass('showDeletebutton');
+	},
+
+	onMouseout: function(e) {
+		'use strict';
+		$(e.target).children('.deleteButton').removeClass('showDeletebutton');
+	},
+
+	onClick: function() {
+		'use strict';
+		this.model.destroy();
 	},
 	
 	setCompleted: function(target) {
